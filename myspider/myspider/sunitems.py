045e9a0ca681b337c7ae140e94dfe8bd9266d183
ShapeLovePeople <1192293855@qq.com>
2019-04-07 23:48:51 +0800

    init sun

---
+++ myspider/myspider/sunitems.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+#在 Items中预先定义好要爬点字段
+class SunspiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
